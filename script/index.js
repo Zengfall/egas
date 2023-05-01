e392121c7def435f38ff8a099bca9046d86dee8a
Senghor <lordzeng@outlook.fr>
2023-05-01 16:35:36 +0200

    javascript done

--- script/index.js
+++ script/index.js
@@ -1,10 +1,26 @@
-const chevron = document.querySelector('.chevron a');
+//Chevron
+const chevron = document.querySelector('.chevron ');
 
 chevron.addEventListener('click', () => {
   const activites = document.querySelector('#activites');
   activites.scrollIntoView({ behavior: 'smooth' });
 });
 
+// Texte QSE
+
+  const qseImage = document.getElementById("qse-image");
+  const qseTexte = document.querySelector(".qse-texte");
+  const qseTexteClose = document.querySelector(".qse-texte-close");
+
+  qseImage.addEventListener("click", () => {
+    qseTexte.style.display = "block";
+  });
+
+  qseTexteClose.addEventListener("click", () => {
+    qseTexte.style.display = "none";
+  });
+
+ //Bouton back-to-top
 var backToTopButton = document.querySelector(".back-to-top");
 
 window.addEventListener("scroll", scrollFunction);
