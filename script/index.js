07c7742a8624b6d709dc452c717cc2bda369fcdf
Senghor <lordzeng@outlook.fr>
2023-06-23 00:34:32 +0200

    Formulaire d'envoi done

--- script/index.js
+++ script/index.js
@@ -17,6 +17,29 @@ function closeNav() {
   sidenav.classList.remove("active")
 }
 });
+// Ajouter un gestionnaire d'événements sur les liens du menu pour faire défiler la page
+  var menuLinks = document.querySelectorAll(".sidenav a");
+  menuLinks.forEach(function(link) {
+    link.addEventListener("click", function(event) {
+      event.preventDefault(); // Empêcher la redirection par défaut
+
+      // Récupérer l'identifiant de la partie cible
+      var targetId = link.getAttribute("href").substring(1);
+
+      // Trouver l'élément cible dans le document
+      var targetElement = document.getElementById(targetId);
+
+      // Faire défiler la page jusqu'à l'élément cible
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth" });
+      }
+
+      // Fermer le menu après le clic sur un lien
+      sidenav.classList.remove("active");
+    });
+  });
+
+
 //Chevron
 const chevron = document.querySelector('.chevron ');
 
@@ -61,6 +84,47 @@ backToTopButton.addEventListener("click", backToTop);
 function backToTop() {
   window.scrollTo(0, 0);
 }
+//Formulaire de contact
+
+function sendMail() {
+  var params ={
+   nom : document.getElementById("nom").value,
+   prenom : document.getElementById("prenom").value,
+   objet : document.getElementById("objet").value,
+   email : document.getElementById("email").value,
+   message : document.getElementById("message").value,
+   };
+
+  const serviceId="service_vkwaujy";
+  const templateId="template_hkukxq6"; 
+
+  emailjs.send(serviceId, templateId, params)
+  .then((res) => {
+    document.getElementById("nom").value = "";
+    document.getElementById("prenom").value = "";
+    document.getElementById("objet").value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+    console.log(res);
+    alert("Votre message a été envoyé avec succés");
+  })
+  .catch((err)=>console.log(err));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Ciblez le formulaire
+  var form = document.getElementById('myForm');
+
+  // Ajoutez un écouteur d'événement pour l'événement "submit"
+  form.addEventListener('submit', function(event) {
+    // Empêchez le comportement par défaut du formulaire
+    event.preventDefault();
+
+    // Appelez la fonction sendMail()
+    sendMail();
+  });
+});
+
 
 
 
